eb233a608ccb6c52d9614363c807121286a1dec9
Judeu <joaogt13@gmail.com>
2022-05-15 16:49:00 -0300

    Close issue #40

---
+++ Frontend/Frontend/app/src/main/java/com/rubick/tinli/Controllers/MainActivity.java
@@ -0,0 +1,34 @@
+package com.rubick.tinli.Controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.rubick.tinli.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button startSeason = findViewById(R.id.start_season);
+        Button registerBt = findViewById(R.id.finish_bt);
+
+        startSeason.setOnClickListener(v ->{
+            Intent login = new Intent(getApplicationContext(), Login.class);
+            startActivity(login);
+        });
+
+        registerBt.setOnClickListener(v ->{
+            Intent register = new Intent(getApplicationContext(), Register.class);
+            startActivity(register);
+        });
+    }
+}
